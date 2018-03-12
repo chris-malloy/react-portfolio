030cb3b770cc852814c1f9a67b36b14824eb58f0
chris-malloy <chrismalloymusic@gmail.com>
2018-03-12 15:39:18 -0400

    slideout func working

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Navbar } from 'react-materialize';
+import { Button, Navbar, NavItem, SideNav, SideNavItem } from 'react-materialize';
 
 const MyNavbar = () => {
-
   return (
     <Navbar brand='Chris Malloy' right>
+      <NavItem href='/'>Projects</NavItem>
+      <NavItem href='/'>Skills</NavItem>
+      <NavItem href='/'>Bio</NavItem>
+      <NavItem href='/'>Contact</NavItem>
     </Navbar>
   );
 };
